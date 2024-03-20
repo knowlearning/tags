b5c9791f0dc532d3879b92d6911f6001913c3583
Jason Ford <fordjason@gmail.com>
2024-03-19 17:43:59 -0700

    Add vuetify

--- src/main.js
+++ src/main.js
@@ -2,9 +2,36 @@ import Agent from '@knowlearning/agents/browser.js'
 import { createApp } from 'vue'
 import './style.css'
 
+import 'vuetify/styles'
+import '@fortawesome/fontawesome-free/css/all.css'
+import { createVuetify } from 'vuetify'
+import { aliases, fa } from 'vuetify/iconsets/fa'
+import vuetifyKnowLearningTheme from './vuetify-knowlearning-theme.js'
+
+//  TODO: trim down imports
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+
 import App from './App.vue'
 
 window.Agent = Agent
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  theme: {
+    defaultTheme: 'vuetifyKnowLearningTheme',
+    themes: {
+      vuetifyKnowLearningTheme,
+    },
+  },
+  icons: {
+    defaultSet: 'fa',
+    aliases,
+    sets: { fa }
+  }
+})
 
 Agent
   .environment()
@@ -12,4 +39,6 @@ Agent
     if (provider !== 'google') Agent.login()
   })
 
-createApp(App).mount('#app')
+createApp(App)
+  .use(vuetify)
+  .mount('#app')
