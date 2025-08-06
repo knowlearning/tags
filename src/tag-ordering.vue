a75a7169bd2412fbbf109d137a1bdf8e9a95ff52
Jason Ford <fordjason@gmail.com>
2025-08-06 12:57:03 -0700

    Scaffold update for ordering

--- src/tag-ordering.vue
+++ src/tag-ordering.vue
@@ -6,10 +6,18 @@
   })
 
   const taggings = await Agent.query('taggings-targeting-taggings', [props.partition, props.orderTag, props.tag])
+
+  async function order(target_tagging) {
+    const tags = await Agent.state('tags')
+    if (!tags[props.orderTag]) tags[props.orderTag] = {}
+    tags[props.orderTag][target_tagging] = { partition: props.partition, value: { woo: '?' } }
+  }
+
 </script>
 
 <template>
-  <pre>
-    {{taggings}}
-  </pre>
+  <div v-for="tagging in taggings">
+    {{ tagging }}
+    <v-btn @click="order(tagging.target_tagging)">add tagging for thing</v-btn>
+  </div>
 </template>
